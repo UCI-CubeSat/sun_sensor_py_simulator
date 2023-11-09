878e308aa3cd4a6e483c8ffd800de0c9bd72cdba
Abhi Arya <89806154+abhi-arya1@users.noreply.github.com>
2023-11-09 14:57:14 -0800

    Update and rename angle_analysis.py to simulator.py

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import vector_math as vm 
+import vector_math as vm
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection 
 
 # constants
 sensor_width = 10.0  
@@ -56,22 +57,86 @@ print(f'Spherical Vector (Satellite to Sun): {(spoint := sat_to_sun).r, np.degre
 print(f'Cartesian Vector (Satellite to Sun): {(point := vm.sph_to_cart(sat_to_sun)).x, point.y, point.z}')
 print(f'Estimated azimuth angle of the heat source within {fov_degrees} degrees FOV at a height of {height} mm: {azimuth} degrees')
 
+roll_angle = 0 #float(input('Roll Angle: '))
 
-EVA = vm.normalize(vm.cartVec3(point.x, point.y, 0))
-AVA = vm.normalize(vm.cartVec3(0, point.y, point.z))
-elev_quaternion = vm.Quaternion(np.cos(spoint.theta/2), (sin := np.sin(spoint.theta/2))*EVA.x, sin*EVA.y, sin*EVA.z)
-azi_quaternion = vm.Quaternion(np.cos(spoint.phi/2), (sin := np.sin(spoint.phi/2))*AVA.x, sin*AVA.y, sin*AVA.z)
-#rotation = elev_quaternion.rotate_point((1, 1, 1))
-print(elev_quaternion)
-print(azi_quaternion)
+QuaternionX = vm.Quaternion(
+    np.cos(np.radians(roll_angle/2)), 
+    (sin:=np.sin(np.radians(roll_angle/2)))*1, 
+    sin*0,
+    sin*0)
+QuaternionY = vm.Quaternion(
+    np.cos((np.radians(90-np.degrees(spoint.phi)))/2), 
+    (sin:=np.sin((np.radians(90-np.degrees(spoint.phi)))/2))*0, 
+    sin*1, 
+    sin*0)
+QuaternionZ = vm.Quaternion(
+    np.cos(np.degrees(np.radians(spoint.theta)/2)), 
+    (sin:=np.sin(np.radians(np.degrees(spoint.theta))/2))*0,
+    sin*0, 
+    sin*1)
+
+def generate_cube_vertices(side_length):
+    half_length = side_length / 2
+    vertices = np.array([
+        [-half_length, -half_length, -half_length],
+        [half_length, -half_length, -half_length],
+        [half_length, half_length, -half_length],
+        [-half_length, half_length, -half_length],
+        [-half_length, -half_length, half_length],
+        [half_length, -half_length, half_length],
+        [half_length, half_length, half_length],
+        [-half_length, half_length, half_length]
+    ])
+    return vertices
+
+fig = plt.figure(2)
+ax = fig.add_subplot(111, projection='3d')
+
+# Define the initial cube vertices
+side_length = 2.0
+cube_vertices = generate_cube_vertices(side_length)
+rotated_vertices = [QuaternionX.rotate_point(vertex) for vertex in cube_vertices]
+rotated_vertices = [QuaternionY.rotate_point(vertex) for vertex in rotated_vertices]
+rotated_vertices = [QuaternionZ.rotate_point(vertex) for vertex in rotated_vertices]
+
+# Plot the rotated cube
+rotated_vertices = np.array(rotated_vertices)
+ax.scatter(rotated_vertices[:, 0], rotated_vertices[:, 1], rotated_vertices[:, 2])
+
+cube_edges = [
+    (0, 1), (1, 2), (2, 3), (3, 0),
+    (4, 5), (5, 6), (6, 7), (7, 4),
+    (0, 4), (1, 5), (2, 6), (3, 7)
+]
+
+colors = ['r', 'g', 'b', 'c', 'm', 'y']
+cube_faces = [
+    [rotated_vertices[0], rotated_vertices[1], rotated_vertices[2], rotated_vertices[3], colors[0]],
+    [rotated_vertices[4], rotated_vertices[5], rotated_vertices[6], rotated_vertices[7], colors[1]],
+    [rotated_vertices[0], rotated_vertices[4], rotated_vertices[7], rotated_vertices[3], colors[2]],
+    [rotated_vertices[1], rotated_vertices[5], rotated_vertices[6], rotated_vertices[2], colors[3]],
+    [rotated_vertices[0], rotated_vertices[1], rotated_vertices[5], rotated_vertices[4], colors[4]],
+    [rotated_vertices[3], rotated_vertices[2], rotated_vertices[6], rotated_vertices[7], colors[5]]
+]
+
+# Plot the rotated cube faces
+for face in cube_faces:
+    vertices = face[:-1]
+    color = face[-1]
+    vertices = np.array(vertices)
+    ax.add_collection3d(Poly3DCollection([vertices], alpha=0.25, facecolor=color))
+
+ax.quiver(0, 0, 0, 2*point.x, 2*point.y, 2*point.z, color='r', label='Sat to Sun')
+
+# Set axis labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title(f'{np.degrees(spoint.theta)}* Azimuth, {np.degrees(spoint.phi)}* Elevation CubeSat')
 
 
-
-
-
-
-# visualize data 
-plt.figure()
+# # visualize data 
+plt.figure(1)
 plt.imshow(solar_intensity_at_location, cmap='hot', extent=(-sensor_width / 2, sensor_width / 2, -sensor_width / 2, sensor_width / 2))
 plt.xlabel('X Position (mm)')
 plt.ylabel('Y Position (mm)')
